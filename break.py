4d677a4849c8b0f5ab20547049869a5dca739bf0
Liluan1 <37143470+Liluan1@users.noreply.github.com>
2019-02-19 22:42:14 +0800

    vscode_init

--- break.py
+++ break.py
@@ -1,7 +1,7 @@
-#coding:utf-8
-import string
+# coding:utf-8
+
 while True:
-    s = raw_input() #python2中的input()只能输入数字，raw_input()只输入字符串
+    s = input()  # python2中的input()只能输入数字，raw_input()只输入字符串
     if s == 'quit':
         break
-    print len(s)
+    print(len(s))
